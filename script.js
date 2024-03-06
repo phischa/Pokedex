415e38c51bcd9fd49c999adc6ad23ee8ef7b063f
phischa <ph.git@mailbox.org>
2024-03-06 21:17:21 +0100

    fonts added

--- script.js
+++ script.js
@@ -12,4 +12,5 @@ async function loadPokemon() {
 
 function renderPokemonInfo(){
     document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+    document.getElementById('pokemonImage').src = currentPokemon['sprites']['front_default'];
 }
